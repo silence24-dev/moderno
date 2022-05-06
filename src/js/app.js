da7ddc0bcdab145d56d7cd9c779f76f7f715616c
silence24-dev <alexey.krsk24@gmail.com>
2022-05-07 01:12:17 +0700

    added product and slider

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,20 @@ flsFunctions.isWebp();
 import $ from "jquery";
 import "slick-carousel";
 import mixitup from 'mixitup';
+import "./modules/rating.js"
 
 
-$(function(){
+
+$(function () {
+
 	var mixer = mixitup('.products__inner-box');
+
+	$('.product-slider__inner').slick({
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		arrows: false,
+	});
+
 });
+
