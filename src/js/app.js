2b8f1cea57b5d3e6da8b9f66c05566b19efe9cdb
silence24-dev <alexey.krsk24@gmail.com>
2022-05-15 04:12:08 +0700

    added page  product-one, without aside

--- src/js/app.js
+++ src/js/app.js
@@ -30,6 +30,15 @@ $(function () {
 		$('.header__box').toggleClass('active');
 	});
 
+	$('.product-one__tabs .tab').on('click', function (event) {
+		var id = $(this).attr('data-id');
+		$('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.product-one__tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#' + id).addClass('active-tab').fadeIn();
+		return false;
+	});
+
 
 	/* писать самым последним потому что он в DOM только на главной странице */
 	var mixer = mixitup('.products__inner-box');
